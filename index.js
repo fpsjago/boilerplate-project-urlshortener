40a47196fb4f2b1ad3d522988d7db6c894f21086
fpsjago <105124565+fpsjago@users.noreply.github.com>
2023-01-21 16:51:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
-
-// Basic Configuration
-const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }))
+
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +13,51 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+
+const fullUrl = []
+const shortUrl = []
+
+
+app.post('/api/shorturl/', (req, res) => {
+  const url = req.body.url
+  const index = fullUrl.indexOf(url)
+
+  if (!url.includes("https://") && !url.includes("http://")) {
+    return res.json({ error: 'Invalid url' })
+  }
+
+  if (index < 0) {
+
+    fullUrl.push(url)
+    shortUrl.push(shortUrl.length)
+
+    return res.json({
+      original_url: url,
+      short_url: shortUrl.length - 1
+    })
+  }
+
+  return res.json({
+    original_url: url,
+    short_url: shortUrl[index]
+  })
+})
+
+app.get('/api/shorturl/:shorturl', (req, res) => {
+  const short = parseInt(req.params.shorturl)
+
+  const foundIndex = shortUrl.indexOf(short)
+
+  if (foundIndex < 0) {
+    return res.json({ "error": "Nos short URL found for the given input" })
+  }
+
+  res.redirect(fullUrl[foundIndex])
+})
+
+
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
